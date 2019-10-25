d1d9e306c0a9a7fd44669899692443ca7fa538c0
Egor Shum <89135509130@mail.ru>
2019-10-25 20:46:12 +0700

    Lesson 69

--- mapsApp/src/User.ts
+++ mapsApp/src/User.ts
@@ -1,16 +1,22 @@
 import faker from 'faker';
+import { Mappable } from './CustomMap';
 
-export class User {
+// it tells to TS 'we want to make sure that the instance of this class sutisfies all the properties required by the Mappable interface'
+export class User implements Mappable {
   name: string;
   location: {
     lat: number;
     lng: number;
   };
+  color: string = 'red';
   constructor() {
     this.name = faker.name.firstName();
     this.location = {
       lat: parseFloat(faker.address.latitude()),
       lng: parseFloat(faker.address.longitude())
     };
+  }
+  markerContent(): string {
+    return `User Name: ${this.name}`;
   }
 }
